df4c8c210f281e2e1c74dae97bdbeec7dab5afca
Zimmer-PedroPaulo <az.zimmer@outlook.com>
2024-12-20 11:58:47 -0300

    Desafio POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "+5511234567890", modelo: "Nokia 701", imei: "9823692362359875", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+Console.WriteLine(nokia.Numero);
+
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "+5511234567890", modelo: "Iphone 13", imei: "871386135587035701357", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+Console.WriteLine(iphone.Numero);
